1daa284484949292207eea8641bb69fb81673a8b
mariuscozma11 <mariuscozmabe@gmail.com>
2025-12-11 21:31:48 +0200

    Enhanced animations

--- src/components/hero-section-demo-1.tsx
+++ src/components/hero-section-demo-1.tsx
@@ -79,9 +79,13 @@ export default function HeroSectionOne() {
         <motion.div
           initial={{
             opacity: 0,
+            filter: "blur(4px)",
+            y: 10,
           }}
           animate={{
             opacity: 1,
+            filter: "blur(0px)",
+            y: 0,
           }}
           transition={{
             duration: 0.3,
